037b013a406b2855dada37941f7377eaf296dab5
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-06-29 14:38:09 +0100

    update exercise

--- Warmups/01_monkey_trouble.py
+++ Warmups/01_monkey_trouble.py
@@ -1,7 +1,4 @@
 ### Monkeys a and b. In trouble when both or neither are smiling. Return True when we are in trouble ###
 
 def monkey_trouble(a_smile, b_smile):
-	if a_smile == b_smile:
-		return True
-	else:
-		return False
+	return a_smile == b_smile
