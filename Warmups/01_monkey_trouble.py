496ac844004573e67fc9f387b1999c89e82211e5
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-06-29 14:21:46 +0100

    exrecise 2 update

--- Warmups/01_monkey_trouble.py
+++ Warmups/01_monkey_trouble.py
@@ -1,7 +1,7 @@
 ### Monkeys a and b. In trouble when both or neither are smiling. Return True when we are in trouble ###
 
 def monkey_trouble(a_smile, b_smile):
-	if a_smile == True and b_smile == True or a_smile == False and b_smile == False:
+	if a_smile == b_smile:
 		return True
 	else:
 		return False
