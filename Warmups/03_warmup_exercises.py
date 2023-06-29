90f8f14e43c8f4f2d563cb96d00248b045641d46
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-06-29 14:35:32 +0100

    add exercises

---
+++ Warmups/03_warmup_exercises.py
@@ -0,0 +1,26 @@
+### Warm-up exercises set 1 ###
+
+### Given integer, return absolute difference with 21, but return double difference if n > 21 ###
+
+def diff21(n):
+	if n > 21 :
+		return 2*(n-21)
+	else:
+		return 21-n
+
+
+### If talking and hour is before 7 or after 20, return True ###
+
+def parrot_trouble(talking, hour):
+	if talking == True and (hour < 7 or hour > 20):
+		return True
+	else:
+		return False
+
+### Given 2 integers, return true if one of them is 10 or their sum is 10 ###
+
+def makes10(a,b):
+	if a == 10 or b == 10 or a+b==10:
+		return True
+	else:
+		return False
