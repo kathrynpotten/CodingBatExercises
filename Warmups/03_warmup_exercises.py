0503d7a22824559933cffc4f04b94a82f77e7d6f
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-06-29 14:49:56 +0100

    add exercises

--- Warmups/03_warmup_exercises.py
+++ Warmups/03_warmup_exercises.py
@@ -20,3 +20,33 @@ def parrot_trouble(talking, hour):
 def makes10(a,b):
 	return a == 10 or b == 10 or a+b==10
 
+
+
+### Given an integer, return True if within 10 of 100 or 200 ###
+
+def near_hundred(n):
+	return abs(100-n) <= 10 or abs(200-n) <= 10
+
+
+### Given 2 integers, return True if one negative and one positive, 
+#unless negative==True, when return only if both are negative ###
+
+def pos_neg(a,b,negative):
+	if negative == True:
+		return a < 0 and b < 0
+	else:
+		return a*b < 0 
+
+### Given a string, return the same string with 'not' added in front. 
+#If it already begins with 'not', leave unchanged ### 
+
+def not_string(str):
+	if str[:3] == 'not':
+		return str
+	else:
+		return 'not ' + str
+
+### Given a non-empty string and an integer n, return string with character n removed ###
+
+def missing_char(str,n):
+	return str[:n] + str[n+1:]
